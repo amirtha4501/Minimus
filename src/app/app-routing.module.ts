387b3f9f46992eaab07dfc904ea885b7b82de80a
amirtha4501 <amirtha4501@gmail.com>
2020-12-20 20:29:11 +0530

    Added home and add-card component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AddCardComponent } from './ui/add-card/add-card.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {path: '', component: HomeComponent},
+  // {path: 'add', component: AddCardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
